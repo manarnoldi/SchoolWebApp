4ab374cbce4dba42997ed0b28b97890d063029f5
manarnoldi <manarnoldi@gmail.com>
2024-02-07 18:02:33 +0300

    Initial

---
+++ SchoolWebApp-API/Project.API/Program.cs
@@ -0,0 +1,66 @@
+using Microsoft.EntityFrameworkCore;
+using Project.API.Middlewares;
+using Project.Infrastructure.Data;
+using Project.API.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using BenchmarkDotNet.Running;
+using Project.API.Controllers;
+using BenchmarkDotNet.Configs;
+using Castle.Core.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+string mySqlConnectionStr = builder.Configuration.GetConnectionString("PrimaryDbConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(
+                options => options.UseMySql(mySqlConnectionStr, ServerVersion.AutoDetect(mySqlConnectionStr)));
+
+// Register ILogger service
+builder.Services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.AddSeq(builder.Configuration.GetSection("Seq"));
+});
+
+//Register Services
+builder.Services.RegisterService();
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+    {
+        options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "SchoolWebAPP API Project", Version = "v1" });
+    });
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseRouting(); // Add this line to configure routing
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers(); // Map your regular API controllers
+
+    // Add a custom endpoint for triggering benchmarks
+    endpoints.MapGet("/runbenchmarks", async context =>
+    {
+        // You can run the benchmarks here
+        var summary = BenchmarkRunner.Run<OrderControllerBenchmark>();
+
+        await context.Response.WriteAsync("Benchmarks completed. Check console for results.");
+    });
+});
+
+#region Custom Middleware
+
+app.UseRequestResponseLogging();
+#endregion
+
+
+
+app.Run();
